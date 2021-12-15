a3a4921ee4dd7e61dcc22eb328cbf79d1efcaa8c
AriSyafri <86141992+AriSyafri@users.noreply.github.com>
2021-12-15 22:12:41 +0700

    Add files via upload

---
+++ 2_printAlurEkse/src/pkg2_printalurekse/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2_printalurekse;
+
+/**
+ *
+ * @author Ari
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //ada tiga tipe print :
+        System.out.println("hello world"); //baris ini namanya statement
+        System.out.print("Baris ke-2 : hai juga kang pukis");
+        System.out.print("Baris ke-3 : saya meneruskan baris ketiga");
+        System.out.println("Baris ke 4 : Hallo mahmuy???");
+        System.out.println("Baris ke 5 : cek performa laptop aja ini sambil belajar\n");
+        System.out.print("Baris ke 6 : laptop rasa todoroki\n");
+        System.out.println("Baris ke 7 : Ngeri anjim");
+        
+        //memakai print format
+        System.out.printf("wiro sableng geni %d",212); //%d adalah %desimal
+        
+        
+        
+    }
+    
+}
