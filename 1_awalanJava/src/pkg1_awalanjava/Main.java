c17af8651b00b10ad47ad8d7e8f7884ecb9659fc
AriSyafri <86141992+AriSyafri@users.noreply.github.com>
2021-12-15 22:06:41 +0700

    ini file awal

---
+++ 1_awalanJava/src/pkg1_awalanjava/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1_awalanjava;
+
+/**
+ *
+ * @author Ari
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("java otong surotong");
+        System.out.println("Nama saya otong teman ucup");
+        
+    }
+    
+}
